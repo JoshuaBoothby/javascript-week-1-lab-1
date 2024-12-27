eb785c3ce67db6ef6ac2cb0f5053856f55e4f668
Joshua Boothby <jcboothby95@gmail.com>
2024-12-26 18:40:10 -0600

    fixed the addition in the calculator

--- simple-calculator.js
+++ simple-calculator.js
@@ -15,7 +15,7 @@ let operation = prompt("Enter operation?");
 let result;
 
 if (operation === "+") {
-    result = num1 = num2;
+    result = num1 + num2;
 }
 else if (operation === "-") {
     result = num1 - num2;
